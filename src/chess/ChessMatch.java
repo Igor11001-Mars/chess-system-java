1704180641dddeb7f9920ffc9eb50e5a2ca4d53c
Igor Casburgo <Igor.casburgo@hotmail.com>
2020-05-27 23:05:36 -0300

    Handling exceptions ad clearing screen

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,7 +26,7 @@ public class ChessMatch {
 		return mat;
 	}
 	
-	public ChessPiece perfomChessmove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
